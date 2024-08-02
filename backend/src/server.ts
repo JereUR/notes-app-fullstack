62327c99b4d589ded1128dd3d8aa31fdb4f9531d
JereUR <jeremias.jdv@gmail.com>
2024-08-02 20:34:21 -0300

    feat: Imports updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,15 @@
+import app from './app'
+import env from './util/validateEnv'
 import mongoose from 'mongoose'
 
-import env from './util/validateEnv'
-import app from './app'
-
-const PORT = env.PORT
+const port = env.PORT
 
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
+    console.log('Mongoose connected')
+    app.listen(port, () => {
+      console.log('Server running on port: ' + port)
     })
   })
-  .catch((error) => console.error('Error connecting to MongoDB:', error))
+  .catch(console.error)
