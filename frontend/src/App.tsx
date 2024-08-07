3491797c1f71925431502783364b488218850475
JereUR <jeremias.jdv@gmail.com>
2024-08-06 22:11:48 -0300

    feat: Form for add note added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 import styles from './styles/NotePage.module.css'
+import stylesUtils from './styles/utils.module.css'
 import { Note as NoteModel } from './models/note'
 import Note from './components/Note'
 import * as NotesApi from './network/notes_api'
+import AddNoteDialog from './components/AddNoteDialog'
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([])
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState<boolean>(false)
 
   useEffect(() => {
     async function getNotes() {
@@ -25,6 +28,12 @@ function App() {
 
   return (
     <Container>
+      <Button
+        className={`mb-4 ${stylesUtils.blockCenter}`}
+        onClick={() => setShowAddNoteDialog(true)}
+      >
+        Add new note
+      </Button>
       <Row xs={1} md={2} xl={3} className="g-4">
         {notes.map((note) => (
           <Col key={note._id}>
@@ -32,6 +41,15 @@ function App() {
           </Col>
         ))}
       </Row>
+      {showAddNoteDialog && (
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote])
+            setShowAddNoteDialog(false)
+          }}
+        />
+      )}
     </Container>
   )
 }
---
+++ frontend/src/components/AddNoteDialog.tsx
@@ -0,0 +1,76 @@
+import { Button, Form, Modal } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+
+import { Note } from '../models/note'
+import { NoteInput } from '../network/notes_api'
+import * as NotesApi from '../network/notes_api'
+
+interface AddNoteDialogProps {
+  onDismiss: () => void
+  onNoteSaved: (note: Note) => void
+}
+
+const AddNoteDialog = ({ onDismiss, onNoteSaved }: AddNoteDialogProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<NoteInput>()
+
+  async function onSubmit(input: NoteInput) {
+    try {
+      const noteResponse = await NotesApi.createNote(input)
+      onNoteSaved(noteResponse)
+    } catch (error) {
+      console.error(error)
+      alert(error)
+    }
+  }
+
+  return (
+    <Modal show onHide={onDismiss}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Note</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form id="addNotForm" onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group controlId="title" className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter title"
+              isInvalid={!!errors.title}
+              {...register('title', {
+                required: 'Required'
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.title?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="text">
+            <Form.Label>Text</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Enter text"
+              {...register('text')}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          type="submit"
+          className="btn btn-primary"
+          form="addNotForm"
+          disabled={isSubmitting}
+        >
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default AddNoteDialog
