ab5d101217565ef9c6d29ae40adc7d71b4da909e
JereUR <jeremias.jdv@gmail.com>
2024-08-06 21:28:35 -0300

    feat: Format date function added

---
+++ frontend/src/utils/formatDate.ts
@@ -0,0 +1,12 @@
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString)
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }
+  return date.toLocaleString('en-US', options)
+}
