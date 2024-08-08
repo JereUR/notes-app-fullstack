8079dc0710b0b5d850c3931dedf1a1dfd64d204e
JereUR <jeremias.jdv@gmail.com>
2024-08-07 21:01:09 -0300

    feat: Edit and delete function added

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,15 +1,24 @@
 import { Card } from 'react-bootstrap'
+import { MdDelete } from 'react-icons/md'
 
 import styles from '../styles/Note.module.css'
+import stylesUtils from '../styles/utils.module.css'
 import { Note as NoteModel } from '../models/note'
 import { formatDate } from './../utils/formatDate'
 
 interface NoteProps {
   note: NoteModel
+  onNoteClicked: (note: NoteModel) => void
+  onDeleteNoteClicked: (note: NoteModel) => void
   className?: string
 }
 
-const Note = ({ note, className }: NoteProps) => {
+const Note = ({
+  note,
+  onNoteClicked,
+  onDeleteNoteClicked,
+  className
+}: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note
 
   let createdUpdatedText: string
@@ -20,9 +29,21 @@ const Note = ({ note, className }: NoteProps) => {
   }
 
   return (
-    <Card className={`${styles.noteCard} ${className}`}>
+    <Card
+      className={`${styles.noteCard} ${className}`}
+      onClick={() => onNoteClicked(note)}
+    >
       <Card.Body className={styles.cardBody}>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className={stylesUtils.flexCenter}>
+          {title}
+          <MdDelete
+            className="text-danger ms-auto"
+            onClick={(e) => {
+              onDeleteNoteClicked(note)
+              e.stopPropagation()
+            }}
+          />
+        </Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
