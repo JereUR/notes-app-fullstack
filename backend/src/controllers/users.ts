0a33bd187b6ba0e94296577ee618f6b96b82796c
JereUR <jeremias.jdv@gmail.com>
2024-08-07 22:17:26 -0300

    Feat: Logout controller added

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -117,3 +117,13 @@ export const login: RequestHandler<
     next(error)
   }
 }
+
+export const logout: RequestHandler = async (req, res, next) => {
+  req.session.destroy((error) => {
+    if (error) {
+      next(error)
+    } else {
+      res.sendStatus(200)
+    }
+  })
+}
