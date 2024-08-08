fc48104218268ba1711157fee2e92a092aae5c7c
JereUR <jeremias.jdv@gmail.com>
2024-08-07 22:17:43 -0300

    feat: Secret session env added

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -3,5 +3,6 @@ import { port, str } from 'envalid/dist/validators'
 
 export default cleanEnv(process.env, {
   PORT: port(),
-  MONGO_CONNECTION_STRING: str()
+  MONGO_CONNECTION_STRING: str(),
+  SESSION_SECRET: str()
 })
