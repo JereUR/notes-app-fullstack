fc79ef346c36cf48827ebe5107d4127446e551da
JereUR <jeremias.jdv@gmail.com>
2024-08-07 22:14:17 -0300

    feat: Login and authenticated user action added

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -4,6 +4,24 @@ import bcrypt from 'bcrypt'
 
 import UserModel from '../models/user'
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+  const authenticatedUserId = req.session.userId
+
+  try {
+    if (!authenticatedUserId) {
+      throw createHttpError(401, 'Not authenticated')
+    }
+
+    const user = await UserModel.findById(authenticatedUserId)
+      .select('+email')
+      .exec()
+    if (!user) throw createHttpError(404, 'User not found')
+    res.status(200).json(user)
+  } catch (error) {
+    next(error)
+  }
+}
+
 interface SignUpBody {
   username?: string
   email?: string
@@ -51,7 +69,50 @@ export const signUp: RequestHandler<
       password: passwordHashed
     })
 
+    req.session.userId = newUser._id
+
     res.status(201).json(newUser)
+  } catch (error) {
+    next(error)
+  }
+}
+
+interface LoginBody {
+  username?: string
+  password?: string
+}
+
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  LoginBody,
+  unknown
+> = async (req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    if (!username || !password) {
+      throw createHttpError(400, 'Parameters missing')
+    }
+
+    const user = await UserModel.findOne({ username })
+      .select('+password +email')
+      .exec()
+
+    if (!user) {
+      throw createHttpError(401, 'Invalid credentials')
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw createHttpError(401, 'Invalid credentials')
+    }
+
+    req.session.userId = user._id
+
+    res.status(201).json(user)
   } catch (error) {
     next(error)
   }
