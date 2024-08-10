6b46977c610876603d647a4b0eb0cd7dc261d2e0
JereUR <jeremias.jdv@gmail.com>
2024-08-10 20:58:54 -0300

    feat: Links added with react-router-dom

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
 import { User } from '../models/user'
 import NavBarLogggedInView from './NavBarLogggedInView'
 import NavBarLoggedOutView from './NavBarLoggedOutView'
@@ -19,9 +21,16 @@ const NavBar = ({
   return (
     <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">Notes App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Notes App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link as={Link} to="/privacy">
+              Privacy
+            </Nav.Link>
+          </Nav>
           <Nav className="ms-auto">
             {loggedInUser ? (
               <NavBarLogggedInView
