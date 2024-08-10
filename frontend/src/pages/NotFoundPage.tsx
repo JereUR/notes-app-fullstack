2126d3c3dfd44133a9b6cc7a7ac486688e80b47e
JereUR <jeremias.jdv@gmail.com>
2024-08-10 20:59:55 -0300

    feat: Page not found component added

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <a href="/">Go back to the home page</a>
+    </div>
+  )
+}
+
+export default NotFoundPage
