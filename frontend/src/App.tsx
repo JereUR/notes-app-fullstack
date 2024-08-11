a31f1ce0a77eb3a97b8425922c094138d07c5a49
JereUR <jeremias.jdv@gmail.com>
2024-08-10 21:00:39 -0300

    feat: Wrapped components itno BrowserRouter from react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
-import { Container } from 'react-bootstrap'
 import { useEffect, useState } from 'react'
 
-import styles from './styles/NotePage.module.css'
+import styles from './styles/App.module.css'
 import SignUpModal from './components/SignUpModal'
 import LoginModal from './components/LoginModal'
 import NavBar from './components/NavBar'
 import { User } from './models/user'
 import * as UsersApi from './network/users_api'
-import NotesPageLoggedInView from './components/NotesPageLoggedInView'
-import NotesPageLoggedOutView from './components/NotesPageLoggedOutView'
+import NotesPage from './pages/NotesPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import PrivacyPage from './pages/PrivacyPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null)
@@ -28,21 +30,24 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <NavBar
-        loggedInUser={loggedInUser}
-        onLoginClicked={() => setShowLoginModal(true)}
-        onSignUpClicked={() => setShowSignUpModal(true)}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
-      />
-      <Container className={styles.notesPage}>
-        <>
-          {loggedInUser ? (
-            <NotesPageLoggedInView />
-          ) : (
-            <NotesPageLoggedOutView />
-          )}
-        </>
+    <BrowserRouter>
+      <div>
+        <NavBar
+          loggedInUser={loggedInUser}
+          onLoginClicked={() => setShowLoginModal(true)}
+          onSignUpClicked={() => setShowSignUpModal(true)}
+          onLogoutSuccessful={() => setLoggedInUser(null)}
+        />
+        <Container className={styles.pageContainer}>
+          <Routes>
+            <Route
+              path="/"
+              element={<NotesPage loggedInUser={loggedInUser} />}
+            />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </Container>
         {showSignUpModal && (
           <SignUpModal
             onDismiss={() => setShowSignUpModal(false)}
@@ -61,8 +66,8 @@ function App() {
             }}
           />
         )}
-      </Container>
-    </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
