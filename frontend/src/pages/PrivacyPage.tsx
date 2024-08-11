d6a15a0f7ea22231144d375d381850607b8ea184
JereUR <jeremias.jdv@gmail.com>
2024-08-10 21:00:12 -0300

    feat: Page for policy/privacy component added

---
+++ frontend/src/pages/PrivacyPage.tsx
@@ -0,0 +1,17 @@
+const PrivacyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is a placeholder for the privacy policy.</p>
+      <p>
+        Please note that this app does not collect or store any personal data.
+      </p>
+      <p>
+        For more information about the data we collect, you can refer to our
+        Terms of Service.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPage
