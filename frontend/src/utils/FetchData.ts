76eadd936d768ba34b0e14e7b0a2fbe036b33a53
JereUR <jeremias.jdv@gmail.com>
2024-08-10 21:12:32 -0300

    feat: Custom errors used

--- frontend/src/utils/FetchData.ts
+++ frontend/src/utils/FetchData.ts
@@ -1,3 +1,10 @@
+import {
+  ConflictError,
+  NotFoundError,
+  ServerError,
+  UnauthorizedError
+} from '../errors/http_errors'
+
 export async function fecthData(input: RequestInfo, init?: RequestInit) {
   try {
     const response = await fetch(input, init)
@@ -6,7 +13,20 @@ export async function fecthData(input: RequestInfo, init?: RequestInit) {
     } else {
       const errorBody = await response.json()
       const errorMessage = errorBody.error
-      throw Error(errorMessage)
+
+      if (response.status === 401) {
+        throw new UnauthorizedError(errorMessage)
+      } else if (response.status === 409) {
+        throw new ConflictError(errorMessage)
+      } else if (response.status === 404) {
+        throw new NotFoundError(errorMessage)
+      } else if (response.status === 500) {
+        throw new ServerError(errorMessage)
+      } else {
+        throw new Error(
+          `Request failed with status: ${response.status} message: ${errorMessage}`
+        )
+      }
     }
   } catch (error) {
     console.error('Error:', error)
